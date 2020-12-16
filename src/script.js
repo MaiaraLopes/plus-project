f2affa30ecb99ca1a50f27a054197398ea64fe39
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2020-12-16 19:26:07 +0000

    Javascript update with current temperature

---
+++ src/script.js
@@ -0,0 +1,18 @@
+function displayTemperature(response){
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = document.querySelector = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "76c8b94d63a7e1828617006132974e2d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
