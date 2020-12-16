d2633828709fa7ddb4555952119591e642aac073
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2020-12-16 21:54:48 +0000

    Tried to implement search bar

--- src/script.js
+++ src/script.js
@@ -31,8 +31,20 @@ dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
 }
 
+function search(city){
 let apiKey = "76c8b94d63a7e1828617006132974e2d";
-let city = "Lisbon"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
