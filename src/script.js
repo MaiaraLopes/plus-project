de45815aba20d7f3ba57340dae7574f2e53c1635
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2020-12-18 11:54:54 +0000

    Updated icon URL to wok on mobile

--- src/script.js
+++ src/script.js
@@ -30,7 +30,7 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
-iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 minTempElement.innerHTML = Math.round(response.data.main.temp_min);
 maxTempElement.innerHTML = Math.round(response.data.main.temp_max); 
 }
